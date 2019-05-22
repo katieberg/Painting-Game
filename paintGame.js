31ed143410d1b00b5ab5903e7e10a0f7e76faed0
unknown <kberg1@email.arizona.edu>
2019-05-22 12:14:43 -0700

    reformatted page

--- paintGame.js
+++ paintGame.js
@@ -1,12 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var solutionMatrix=[[0,1,1,1,0],[1,1,1,1,1],[1,1,1,1,1],[0,1,1,1,0],[0,0,1,0,0]]
-    var inGameMatrix=[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-    var count=0;
-    var squares = document.getElementsByClassName("square")
-    
+// fetch("Puzzles.json")
+//   .then(response => response.json())
+//   .then(json => console.log(json));
+
+document.addEventListener('DOMContentLoaded', function () {//one issue occurs normally on the first time you try to do unpaint because the mouse is selecting items as it is painting.
+    var solutionMatrix=[[0,1,0,1,0],[1,1,1,1,1],[1,1,1,1,1],[0,1,1,1,0],[0,0,1,0,0]]//could do different colors for each puzzle
+    var solutionCount=0;
+    for(arr of solutionMatrix){//sets solutionCount to the number of painted cells so we can know when to check if the solution is correct.
+        for(el of arr){
+            if(el==1)
+                solutionCount++;
+        }
+    }
+
+    var inGameMatrix=[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]//always all zeros when game begins
+    var paintedCount=0;//number of painted cells - none so far
+    var squares = document.getElementsByClassName("square")//array of all the paintable cells
+    var columnHints = document.getElementsByClassName("speshSquareCol")
+    for(el of columnHints){
+        el.childNodes[0].innerHTML=el.childNodes[0].innerHTML
+        
+    }//this is how we have to make this work BUT i think we can use a function to do this so we can have a local count to iterate over the JSON array.
 
     var startPaint = function(event){
-        count++;
+        paintedCount++;
         event.target.style.backgroundColor="pink";
         var cell = event.target.id
         var row = parseInt(cell[0,1]-2)
@@ -21,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
            
     }
     var startUnpaint = function(event){
-        count--;
+        paintedCount--;
         event.target.style.backgroundColor="white";
         var cell = event.target.id
         var row = parseInt(cell[0,1]-2)
@@ -38,7 +54,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function paint(){//paint is working. while mouse is down it can be dragged to other squares to paint them
         if(event.target.style.backgroundColor!="pink" && event.target.classList.contains("square")){
-            count++;
+            paintedCount++;
             var cell = event.target.id
             var row = cell[0,1]-2
             var col = cell[2,3]-2
@@ -52,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     function unpaint(){
         if(event.target.style.backgroundColor=="pink" && event.target.classList.contains("square")){
-            count--;
+            paintedCount--;
             var cell = event.target.id
             var row = parseInt(cell[0,1]-2)
             var col = parseInt(cell[2,3]-2)
@@ -69,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function () {
         for(el of squares){
             el.removeEventListener("mouseover",paint)
         }
-        if(count==17){
+        if(paintedCount==solutionCount){
             compareToSolution();
         }
         window.removeEventListener("mouseup",stopPaint)
@@ -79,7 +95,7 @@ document.addEventListener('DOMContentLoaded', function () {
         for(el of squares){
             el.removeEventListener("mouseover",unpaint)
         }
-        if(count==17){
+        if(paintedCount==solutionCount){
             compareToSolution();
         }
         window.removeEventListener("mouseup",stopUnpaint)
